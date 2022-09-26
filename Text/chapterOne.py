357b2b7953b37caae63cb0628c028bf59fe7501f
Julio Arellano <secondary.jcav@gmail.com>
2022-09-26 16:12:04 +0200

    parts A through H

--- Text/chapterOne.py
+++ Text/chapterOne.py
@@ -43,3 +43,98 @@ part_C1 = '“We’re heading straight for the monster.” Samuel drummed his fi
 
 C1_options = [{'selector': '1', 'prompt': 'Eat! After all, exploring on an empty belly is no fun.', 'return': '1'},
               {'selector': '2', 'prompt': 'Head out! Fortune favors the bold!', 'return': '2'}]
+
+part_D1 = '“Oh… I’ll eat some food.” Samuel sighed and walked over to Livia, where he accepted some of the food.\n' \
+          'He did have to admit that he was hungry...\n' \
+          '(they had been on the road for the better part of twelve hours trying to make it before dark, and had passed over multiple meals to save on time),\n' \
+          'and he soon devoured a far larger amount of food than he had originally intended to consume.\n' \
+          'As he sat there, though, he became aware of other people slipping past the car and making their way into the woods.\n' \
+          'Some wore gillie suits, some wore dark sets of clothing, some wore typical tourist clothes.\n' \
+          'Some came with night-vision goggles, some with cameras, some with binoculars, and some came with sketchpads.\n' \
+          '“This doesn’t seem to bode well for us.” Samuel murmured as he watched the odd procession.\n' \
+          '“Don’t all these people know it’s illegal to be out here?”\n' \
+          '“I’m sure they do.” Livia soothed. “Now eat!”\n' \
+          '“I’m done eating.” Samuel scowled, having lost his appetite.\n' \
+          '“Then quit pouting.” Livia nodded. “Now you’ll be ready to go out and find… Well… Whatever it is that we’re going to find.”\n' \
+          '“The mothman!”\n' \
+          '“Right.”\n' \
+          'Samuel puffed out his cheeks as he looked around. The sun had already set, blanketing the forest in shadows.\n' \
+          'Suddenly, though, an immense light blazed through the woods, nearly blinding him.\n' \
+          'It looked like a spotlight, the most powerful spotlight he had ever seen, and for a few moments, he had to blink spots out of his eyes.\n' \
+          '“Well?” Livia sighed and shrugged. “Are we going to go investigate, or not?”\n'
+
+D1_options = [{'selector': '1', 'prompt': 'Go check it out! Fortune favors the bold!', 'return': '1'},
+              {'selector': '2', 'prompt': 'Lie in wait!', 'return': '2'}]
+
+part_E1 = '“Come on! We can eat later!” Samuel declared, then turned and raced out into the trees.\n' \
+          'Livia groaned and followed him, though she did eat the rest of her food before joining him. Together, they began making their way through the slowly-darkening woods.\n' \
+          'And… That was when something became clear.\n' \
+          'They were not alone.\n' \
+          '“Hey! Don’t walk through the monster’s path!”\n' \
+          '“You’ll scare away the mothman!”\n' \
+          '“You stepped on my foot!”\n' \
+          'Spread throughout the woods, hidden behind what seemed like every tree and stump, were dozens of novice monster-hunters.\n' \
+          'Some were wearing full gillie suits (these were the people that Samuel tended to step on), and some were simply hiding with cameras.\n' \
+          'Samuel scowled at them all, and held up his hands.\n' \
+          '“I’m sorry, folks, but you all need to leave. We’re Samuel and Livia, the monster hunters of recent memory.”\n' \
+          'A small patch of mushrooms rose up from the ground, and a man with a long beard that looked to have grown into the grass itself hissed at them.\n' \
+          '“I don’t know who you think you are, but-”\n' \
+          'His voice, though, was cut off as a brilliant spotlight blazed through the woods.\n' \
+          'It was more brilliant than anything Samuel had ever seen before, so much so that he was nearly blinded from it.\n' \
+          '“Well?” Livia sighed and shrugged. “Are we going to go investigate, or not?”\n'
+
+E1_options = [{'selector': '1', 'prompt': 'Go check it out! Fortune favors the bold!', 'return': '1'},
+              {'selector': '2', 'prompt': 'Lie in wait!', 'return': '2'}]
+
+part_F1 = '“And so, I hope you can see that we have a very important sort of mission for you two kids.”\n' \
+          'The agent stood at the front of the helicopter, his harsh gaze piercing out from above a twisting mustache about four decades out of style.\n' \
+          '“Have you both read the mission briefing?”\n' \
+          '“Yes!” Samuel spoke up, even though he was fairly certain that Livia had done no such thing.\n' \
+          '“Let’s pretend that I didn’t.” Livia spoke up after a moment. “Give me the details again!”\n' \
+          'The agent looked annoyed, but continued. “You two kids are with the Monster Management Agency!"\n' \
+          '"Currently, you’re on a mission to gather proof that the Mothman exists."\n' \
+          '"There have been hundreds of sightings across Mothman Park over the last few months, but we haven’t found any solid proof that he’s there."\n' \
+          '"Until we have that proof, the agency’s hands are tied. You two are being sent in as unofficial assets."\n' \
+          '"Come out of this alive, and you’ll be made full agents.”\n' \
+          '“What does unofficial mean?” Livia glanced at Samuel.\n' \
+          '“It means that if we do something stupid, they take no responsibility!” Samuel grinned.\n' \
+          '“Right on!” The agent nodded. “Now, where do you two want to parachute in? I assume you at least looked at the map, right?”\n'
+
+F1_options = [{'selector': '1', 'prompt': 'Head for the lake!', 'return': '1'},
+              {'selector': '2', 'prompt': 'Land on the mountain!', 'return': '2'}]
+
+part_G1 = '“We’ll head for the lake!” Samuel nodded. “That seems like a perfect place! Moths like water, right?”\n' \
+          '“No.” Livia shook her head. Samuel stepped past her and tested the straps on his parachute.\n' \
+          'A moment later, the helicopter whirred into position, and Samuel leapt through the open door.\n' \
+          'Livia followed a moment later, and they found themselves plummeting toward the distant and darkening forest below.\n' \
+          'Within only a few moments, they landed on the shore of a small lake, tucked deep within the park.\n' \
+          'As they came down, Samuel disconnected his parachute and let it crumple onto the shore. Livia did the same, and they started looking around.\n' \
+          '“If I was a mothman, where would I hide?” Samuel stroked his chin in thought.\n' \
+          '“Not here.” Livia tapped his shoulder. “Look.”\n' \
+          'He turned and looked in the direction she was pointing, out across the lake.\n' \
+          'It was a lovely little feature of water, dotted with hundreds of turtle heads… Or at least what seemed at first glance to be turtles.\n' \
+          'At a second glance, they were all small snorkels, poking up from the waves. All across the beach, people lay in shallow holes that they had dug, or crouched behind fallen logs\n' \
+          '“Seems we’re a little late.” Livia remarked.\n' \
+          '“It’ll be fine.” Samuel slid into a bush to hide, jumped back out as he found that it was already occupied, and soon found a small patch of ivy with three leaves in every cluster. “Here! We can-”\n' \
+          'As the sky continued to darken, a sudden flash of light, brighter than any spotlight he had ever seen before, filled the forest, and Samuel gasped.\n' \
+          '“Well?” Livia sighed and shrugged. “Are we going to go investigate, or not?”\n'
+
+G1_options = [{'selector': '1', 'prompt': 'Go check it out! Fortune favors the bold!', 'return': '1'},
+              {'selector': '2', 'prompt': 'Lie in wait!', 'return': '2'}]
+
+part_H1 = '“We’ll head for the mountain!” Samuel declared. “That seems like a perfect place! Moths like mountains, right?”\n' \
+          'Livia rolled her eyes and muttered a response, but a moment later, they were flying out the door of the helicopter and were rapidly plummeting toward the distant Earth.\n' \
+          'Samuel’s grin stretched from ear to ear, and when they came crashing down in the middle of the forest, it was altogether too soon.\n'\
+          'He unbuckled himself from the harness and came down, only to hear a muffled grunt of pain as he landed squarely on a bush. Livia came down next to him, and they looked around the area.\n'\
+          '“Get away from me! You’ll scare the monsters away!” A man hissed from within the bush.\n'\
+          '“It’s okay, sir.” Samuel shrugged. “We’re with the Monster Management Agency.”\n'\
+          '“Show me your badge.”Samuel blinked, and the man shooed the two of them away.\n' \
+          'They quickly began looking for another bush to hide behind, but the majority were occupied by dozens of wannabe monster hunters.\n'\
+          'Finally, though, Samuel found a small log that they slid behind.\n' \
+          '“This is pointless.” Livia muttered. “We should be-”\n'\
+          'Suddenly, cutting her off, an immense light blazed through the woods, nearly blinding him.\n'\
+          'It looked like a spotlight, the most powerful spotlight he had ever seen, and for a few moments, he had to blink spots out of his eyes.\n' \
+          '“Well?” Livia sighed and shrugged. “Are we going to go investigate, or not?”\n'
+
+H1_options = [{'selector': '1', 'prompt': 'Go check it out! Fortune favors the bold!', 'return': '1'},
+              {'selector': '2', 'prompt': 'Lie in wait!', 'return': '2'}]
